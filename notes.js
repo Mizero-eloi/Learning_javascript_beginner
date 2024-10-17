3998af737a96557dacd7dc20b58d17c4110a159c
Mizero-eloi <eloimizero123@gmail.com>
2024-10-17 00:40:09 -0800

    initial commits

---
+++ notes.js
@@ -0,0 +1,54 @@
+console.log("Hello world"); // Terminal of the browser
+
+
+// Variables & values
+
+// key words to create variables 
+// let, const, var
+
+let name = "Erica"; //  variable
+
+// console.log(name); // Erica
+
+
+// name = "Benigne";
+// console.log(name) // Benigne
+
+// const x = 5
+// x = 4 // Will give you an error 
+
+// const is let except doesn't change
+
+
+// if (name === "Erica"){
+//     let phone = "iPHONE";
+//     var isGirl = true;
+
+  
+// }
+// console.log(isGirl);
+
+
+
+
+// const language = "Javascript";
+
+
+// ============================================ If conditions ============================================
+
+// if (condition is true ) {this code will run} else {this one will run}
+
+if (name === "Erica"){
+    console.log("I am erica");
+} else if (name === "Benigne"){
+    console.log("I am Benigne");
+}
+ else {
+    console.log ("I am not Erica nor Benigne");
+}
+
+
+x / y // quotient
+% // remainder
+
+num % 3 === 0 // num divisible by 3
